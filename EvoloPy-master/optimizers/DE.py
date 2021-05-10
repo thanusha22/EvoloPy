99b47de83c29569108734be454f02e4a846f3a6a
thanusha22 <41296295+thanusha22@users.noreply.github.com>
2021-05-10 16:44:28 +0530

    Update DE.py

--- EvoloPy-master/optimizers/DE.py
+++ EvoloPy-master/optimizers/DE.py
@@ -1,123 +1,195 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 16 14:19:49 2016
+@author: hossam
+"""
+'''import math
+import numpy
+import random
+import time
+from solution import solution'''
 import random
 import numpy
-import time
+import math
 from solution import solution
+import time
 
 
-# Differential Evolution (DE)
-# mutation factor = [0.5, 2]
-# crossover_ratio = [0,1]
-def DE(objf, lb, ub, dim, PopSize, iters):
+def WOABAT(objf, lb, ub, dim, SearchAgents_no, Max_iter):
 
-    mutation_factor = 0.5
-    crossover_ratio = 0.7
-    stopping_func = None
-
-    # convert lb, ub to array
+    # dim=30
+    # SearchAgents_no=50
+    # lb=-100
+    # ub=100
+    # Max_iter=500
     if not isinstance(lb, list):
-        lb = [lb for _ in range(dim)]
-        ub = [ub for _ in range(dim)]
+        lb = [lb] * dim
+    if not isinstance(ub, list):
+        ub = [ub] * dim
 
-    # solution
+    # initialize position vector and score for the leader
+    Leader_pos = numpy.zeros(dim)
+    Leader_score = float("inf")  # change this to -inf for maximization problems
+
+    # Initialize the positions of search agents
+    Positions = numpy.zeros((SearchAgents_no, dim))
+    for i in range(dim):
+        Positions[:, i] = (
+            numpy.random.uniform(0, 1, SearchAgents_no) * (ub[i] - lb[i]) + lb[i]
+        )
+
+    # Initialize convergence
+    convergence_curve = numpy.zeros(Max_iter)
+    
+    #bat algorithm addition
+    Qmin = 0  # Frequency minimum
+    Qmax = 2  # Frequency maximum
+    
+     # Initializing arrays
+    Q = numpy.zeros(SearchAgents_no)  # Frequency
+    v = numpy.zeros((SearchAgents_no,dim))  # Velocities
+    
+    A1 = 0.5
+    # Loudness  (constant or decreasing)
+    r = 0.5
+    # Pulse rate (constant or decreasing)
+    
+    z = numpy.zeros((SearchAgents_no,dim))
+    z = numpy.copy(Positions)
+    fitness = numpy.zeros(SearchAgents_no)
+
+
+    ############################
     s = solution()
 
-    s.best = float("inf")
-
-    # initialize population
-    population = []
-
-    population_fitness = numpy.array([float("inf") for _ in range(PopSize)])
-
-    for p in range(PopSize):
-        sol = []
-        for d in range(dim):
-            d_val = random.uniform(lb[d], ub[d])
-            sol.append(d_val)
-
-        population.append(sol)
-
-    population = numpy.array(population)
-
-    # calculate fitness for all the population
-    for i in range(PopSize):
-        fitness = objf(population[i, :])
-        population_fitness[p] = fitness
-        # s.func_evals += 1
-
-        # is leader ?
-        if fitness < s.best:
-            s.best = fitness
-            s.leader_solution = population[i, :]
-
-    convergence_curve = numpy.zeros(iters)
-    # start work
-    print('DE is optimizing  "' + objf.__name__ + '"')
+    print('WOABAT is optimizing  "' + objf.__name__ + '"')
 
     timerStart = time.time()
     s.startTime = time.strftime("%Y-%m-%d-%H-%M-%S")
+    ############################
+    
+    for i in range(0, SearchAgents_no):
+        fitness[i] = objf(Positions[i, :])
+    
+    t = 0  # Loop counter
 
-    t = 0
-    while t < iters:
-        # should i stop
-        if stopping_func is not None and stopping_func(s.best, s.leader_solution, t):
-            break
+    # Main loop
+    while t < Max_iter:
+        for i in range(0, SearchAgents_no):
 
-        # loop through population
-        for i in range(PopSize):
-            # 1. Mutation
+            # Return back the search agents that go beyond the boundaries of the search space
 
-            # select 3 random solution except current solution
-            ids_except_current = [_ for _ in range(PopSize) if _ != i]
-            id_1, id_2, id_3 = random.sample(ids_except_current, 3)
+            # Positions[i,:]=checkBounds(Positions[i,:],lb,ub)
+            for j in range(dim):
+                Positions[i, j] = numpy.clip(Positions[i, j], lb[j], ub[j])
 
-            mutant_sol = []
-            for d in range(dim):
-                d_val = population[id_1, d] + mutation_factor * (
-                    population[id_2, d] - population[id_3, d]
-                )
+            # Calculate objective function for each search agent
+            fitness = objf(Positions[i, :])
 
-                # 2. Recombination
-                rn = random.uniform(0, 1)
-                if rn > crossover_ratio:
-                    d_val = population[i, d]
+            # Update the leader
+            if fitness < Leader_score:  # Change this to > for maximization problem
+                Leader_score = fitness
+                # Update alpha
+                Leader_pos = Positions[
+                    i, :
+                ].copy()  # copy current whale position into the leader position
 
-                # add dimension value to the mutant solution
-                mutant_sol.append(d_val)
+        a = 2 - t * ((2) / Max_iter)
+        # a decreases linearly fron 2 to 0 in Eq. (2.3)
 
-            # 3. Replacement / Evaluation
+        # a2 linearly decreases from -1 to -2 to calculate t in Eq. (3.12)
+        a2 = -1 + t * ((-1) / Max_iter)
 
-            # clip new solution (mutant)
-            mutant_sol = numpy.clip(mutant_sol, lb, ub)
+        # Update the Position of search agents
+        for i in range(0, SearchAgents_no):
+            r1 = random.random()  # r1 is a random number in [0,1]
+            r2 = random.random()  # r2 is a random number in [0,1]
 
-            # calc fitness
-            mutant_fitness = objf(mutant_sol)
-            # s.func_evals += 1
+            A = 2 * a * r1 - a  # Eq. (2.3) in the paper
+            C = 2 * r2  # Eq. (2.4) in the paper
 
-            # replace if mutant_fitness is better
-            if mutant_fitness < population_fitness[i]:
-                population[i, :] = mutant_sol
-                population_fitness[i] = mutant_fitness
+            b = 1
+            #  parameters in Eq. (2.5)
+            l = (a2 - 1) * random.random() + 1  #  parameters in Eq. (2.5)
 
-                # update leader
-                if mutant_fitness < s.best:
-                    s.best = mutant_fitness
-                    s.leader_solution = mutant_sol
+            p = random.random()  # p in Eq. (2.6)
 
-        convergence_curve[t] = s.best
+            for j in range(0, dim):
+
+                if p < 0.5:
+                    if abs(A) >= 1 or abs(A) < 1:
+                        rand_leader_index = math.floor(
+                            SearchAgents_no * random.random()
+                        )
+                        X_rand = Positions[rand_leader_index, :]
+                        Q[i] = Qmin + (Qmin - Qmax) * random.random()
+                        v[i, :] = v[i,j] + ( X_rand[j] - Leader_pos[j] ) * Q[i]
+                        z[i, :] = Positions[i, :] + v[i, :]
+                        
+                       
+                        if random.random() > r:
+                            z[i,:] = Leader_pos[j] + 0.001 * numpy.random.randn(dim)
+                        
+                         # Evaluate new solutions
+                        Fnew = objf(z[i, :])
+
+                        '''# Update if the solution improves
+                        if (Fnew <= fitness[i]) and (random.random() < A1):
+                            Positions[i, :] = numpy.copy(z[i, :])
+                            fitness[i] = Fnew'''
+                        
+                        
+                       
+                
+                        #D_X_rand = abs(C * X_rand[j] - Positions[i, j])
+                        #Positions[i, j] = X_rand[j] - A * D_X_rand
+                    '''
+                    elif abs(A) < 1:
+                        rand_leader_index = math.floor(
+                            SearchAgents_no * random.random()
+                        )
+                        X_rand = Positions[rand_leader_index, :]
+                        Q[i] = Qmin + (Qmin-Qmax) * random.random()
+                        v[i,:] = v[i,j]+(X_rand(j)-Leader_pos[j])*Q[i]
+                        z[i,:] = Positions[i:] + v[i,:]
+                        if random.random() > r:
+                            z[i,:] = Leader_pos[j] + 0.001 * numpy.random.randn(dim)
+                        
+                         # Evaluate new solutions
+                        Fnew = objf(z[i, :])
+                         # Update if the solution improves
+                        if (Fnew <= fitness[i]) and (random.random() < A1):
+                            Positions[i, :] = numpy.copy(z[i, :])
+                            fitness[i] = Fnew
+                        
+                        
+                        
+                        #D_Leader = abs(C * Leader_pos[j] - Positions[i, j])
+                        #Positions[i, j] = Leader_pos[j] - A * D_Leader'''
+
+                elif p >= 0.5:
+
+                    distance2Leader = abs(Leader_pos[j] - Positions[i, j])
+                    # Eq. (2.5)
+                    Positions[i, j] = (
+                        distance2Leader * math.exp(b * l) * math.cos(l * 2 * math.pi)
+                        + Leader_pos[j]
+                    )
+
+        convergence_curve[t] = Leader_score
         if t % 1 == 0:
             print(
-                ["At iteration " + str(t + 1) + " the best fitness is " + str(s.best)]
+                ["At iteration " + str(t) + " the best fitness is " + str(Leader_score)]
             )
-
-        # increase iterations
         t = t + 1
 
-        timerEnd = time.time()
-        s.endTime = time.strftime("%Y-%m-%d-%H-%M-%S")
-        s.executionTime = timerEnd - timerStart
-        s.convergence = convergence_curve
-        s.optimizer = "DE"
-        s.objfname = objf.__name__
+    timerEnd = time.time()
+    s.endTime = time.strftime("%Y-%m-%d-%H-%M-%S")
+    s.executionTime = timerEnd - timerStart
+    s.convergence = convergence_curve
+    s.optimizer = "WOABAT"
+    s.objfname = objf.__name__
+    s.best = Leader_score
+    s.bestIndividual = Leader_pos
 
-    # return solution
     return s
